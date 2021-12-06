d6eb86f5a9b4feb028e9a2e1e09365d98e4df06a
kylehovey <khoveyc@gmail.com>
2021-12-05 16:44:43 -0800

    Added evaluation of part one and part two in the same program (Day 5)

--- 2021/5/Day5/src/Day5.jl
+++ 2021/5/Day5/src/Day5.jl
@@ -9,28 +9,27 @@ function parseInput()
     s -> Tuple(split(s, ",") .|> x -> parse(Int, x) + 1)
 end
 
-function ventMap(((from, to), lines...), grid)
+function ventMap(((from, to), lines...), partTwo = true, grid = zeros((1000, 1000)))
   (x, y), (X, Y) = from, to
   dX, dY = to .- from
 
   if dX == 0 || dY == 0
     grid[min(x, X):max(x, X), min(y, Y):max(y, Y)] .+= 1
-  else
+  elseif partTwo
     coords = zip([0:dX; 0:-1:dX], [0:dY; 0:-1:dY]) .|> dV -> from .+ dV
     grid[coords .|> CartesianIndex] .+= 1
   end
 
   if isempty(lines)
-    return
+    return length(grid[grid .> 1])
   end
 
-  ventMap(lines, grid)
+  ventMap(lines, partTwo, grid)
 end
 
 function main()
-  grid = zeros(Int, (1000, 1000))
-  ventMap(parseInput(), grid)
-  length(grid[grid .> 1])
+  println(ventMap(parseInput(), false))
+  println(ventMap(parseInput(), true))
 end
 
 end # module
